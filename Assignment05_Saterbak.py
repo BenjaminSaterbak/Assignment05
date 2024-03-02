eedffd11caf7cb8f503d4d75ecbf0afebda290a3
Benjamin <bjsaterbak8@gmail.com>
2024-03-01 20:18:16 -0500

    Created the performOperation method

--- Assignment05_Saterbak.py
+++ Assignment05_Saterbak.py
@@ -12,13 +12,25 @@ class BasicMathOperations:
         sum = num1 + num2
         print("The Sum is: "+str(sum))
 
-
+    def performOperator(self,num1,num2,operator):
+        outputVal = None
+        if operator == "+":
+            outputVal = num1 + num2
+        elif operator == "-":
+            outputVal = num1 - num2
+        elif operator == "*":
+            outputVal = num1 * num2
+        elif operator == "/":
+            outputVal = num1 / num2
+        else:
+            print("Please enter a valid operator (+,-,*,/)")
+        print(outputVal)
 
 
 Calc = BasicMathOperations()
 
 # Calc.greetUser("Benjamin","Saterbak")
 
-Calc.addNumbers(4,7)
+# Calc.addNumbers(4,7)
 
-
+Calc.performOperator(9,3,"/")

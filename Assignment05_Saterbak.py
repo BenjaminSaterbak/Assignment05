20181313c6081ac08138562401882999c753a5db
Benjamin <bjsaterbak8@gmail.com>
2024-03-01 20:07:03 -0500

    Created the greet user method

--- Assignment05_Saterbak.py
+++ Assignment05_Saterbak.py
@@ -5,12 +5,17 @@ class BasicMathOperations:
     def __init__(self):
         pass
 
+    def greetUser(self,firstName,lastName):
+        print("Welcome to Basic Math Operations, "+str(firstName),str(lastName))
 
 
 
 
 
 
+Calc = BasicMathOperations()
+
+Calc.greetUser("Benjamin","Saterbak")
 
 
 
